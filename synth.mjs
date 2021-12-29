09907b45997087eee59646940c1c8991dab1b599
dbrian57 <daniel.brian@gmail.com>
2021-12-29 10:31:18 -0500

    working song note catch

--- synth.mjs
+++ synth.mjs
@@ -6,19 +6,21 @@ const synth = new window.Tone.Synth().toDestination();
 //attach a click listener to a play button
 document.querySelector('#C4')?.addEventListener('click', async () => {
 	await window.Tone.start()
-	console.log('audio is ready')
   synth.triggerAttackRelease("C4", "8n");
-  let add_note = song.push("C4")
+  let add_note = song.push("C4");
+  console.log(song);
 })
 
 document.querySelector('#C5')?.addEventListener('click', async () => {
 	await window.Tone.start()
-	console.log('audio is ready')
   synth.triggerAttackRelease("C5", "8n");
+  let add_note = song.push("C5");
+  console.log(song);
 })
+
 //create a synth and connect it to the main output (your speakers)
 synth.triggerAttackRelease("C4")
 
-console.log(song)
+
 
 //play a middle 'C' for the duration of an 8th note
