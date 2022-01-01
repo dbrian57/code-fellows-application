26311274e6f466bea3d17a4ab959ffcb2e5ad725
Daniel Brian <daniel.brian@gmail.com>
2022-01-01 11:08:20 -0500

    added proper key event listener. Did some minor clean up and documentation

--- src/synth.mjs
+++ src/synth.mjs
@@ -3,10 +3,21 @@ import * as Tone from './tone/build/tone.js'
 let song = []
 
 const synth = new window.Tone.Synth().toDestination();
-//attach a click listener to a play button
+
+const timer = ms => new Promise(res => setTimeout(res, ms))
 
 // await window.Tone.start();
 // synth.triggerAttackRelease("C4");
+
+
+document.querySelectorAll('.key').forEach(btn=>btn.addEventListener('click', async () => {
+	let note = "id"
+	note = event.srcElement.id;
+	synth.triggerAttackRelease(note, "8n");
+	song.push(note);
+	console.log(song);
+	document.getElementById('screen').innerHTML = song;
+}))
 
 /*
 document.querySelector("button")?.addEventListener('click', async () => {
@@ -19,6 +30,7 @@ document.querySelector("button")?.addEventListener('click', async () => {
 })
 */
 
+/*
 document.querySelector('#C4')?.addEventListener('click', async () => {
 	await window.Tone.start()
   synth.triggerAttackRelease("C4", "8n");
@@ -43,23 +55,19 @@ document.querySelector('#C5')?.addEventListener('click', async () => {
 	document.getElementById('screen').innerHTML = song
 })
 
-document.querySelector('#play')?.addEventListener('click', () => {
+*/
+
+document.querySelector('#play')?.addEventListener('click', async () => {
 	for (var i = 0; i < song.length; i++) {
 	    console.log(song[i]);
 			synth.triggerAttackRelease(song[i], "8n");
-	    //Do something
+	    await timer(500)
 	}
 })
+
 
 document.querySelector('#clear')?.addEventListener('click', () => {
 	song = []
 	document.getElementById('screen').innerHTML = song
 	console.log(song)
 })
-
-
-
-/* Things still to do:
-1) Capture button (key) `id` on click as a variable and insert it into synth trigger
-2) Make a PLAY button that works (need to add a delay function to this)
-*/
