dfd41753e43e61460d3a2411ef23912cf1c1aa23
Daniel Brian <daniel.brian@gmail.com>
2022-01-10 21:35:30 -0500

    minor cleanup and removed unnecessary querySelectors

--- src/scripts/synth.mjs
+++ src/scripts/synth.mjs
@@ -1,8 +1,9 @@
-import * as Tone from '/src/scripts/tone.js'
+import * as Tone from '/src/scripts/tone.js';
 
-let song = []
+let song = [];
 
-
+let songStorage = document.getElementById('synthScreen').innerHTML;
+songStorage = document.getElementById('synthScreen').innerHTML;
 
 if (localStorage.getItem("songStorage")) {
     song = JSON.parse(localStorage.getItem("songStorage"));
@@ -15,47 +16,48 @@ if (localStorage.getItem("songStorage")) {
 const crusher = new window.Tone.BitCrusher(4).toDestination();
 let synth = new window.Tone.Synth().connect(crusher);
 
-const timer = ms => new Promise(res => setTimeout(res, ms))
+const timer = ms => new Promise(res => setTimeout(res, ms));
 
-document.querySelector('#Poly').addEventListener('click', async () => {
+//Synth selection buttons
+document.getElementById('Poly').addEventListener('click', async () => {
 	synth = new window.Tone.PolySynth().connect(crusher);
 	let x = document.getElementsByClassName("synthButton");
 	var i;
 	for (i = 0; i < x.length; i++) {
 	x[i].style.backgroundColor = "transparent";
-	}
-	document.querySelector('#Poly').style.backgroundColor = 'rgba(252, 186, 3, 0.75)';
-})
+	};
+	document.getElementById('Poly').style.backgroundColor = 'rgba(252, 186, 3, 0.75)';
+});
 
-document.querySelector('#Duo').addEventListener('click', async () => {
+document.getElementById('Duo').addEventListener('click', async () => {
 	synth = new window.Tone.DuoSynth().connect(crusher);
 	let x = document.getElementsByClassName("synthButton");
 	var i;
 	for (i = 0; i < x.length; i++) {
 	x[i].style.backgroundColor = "transparent";
-	}
-	document.querySelector('#Duo').style.backgroundColor = 'rgba(252, 186, 3, 0.75)';
-})
+	};
+	document.getElementById('Duo').style.backgroundColor = 'rgba(252, 186, 3, 0.75)';
+});
 
-document.querySelector('#Metal').addEventListener('click', async () => {
+document.getElementById('Metal').addEventListener('click', async () => {
 	synth = new window.Tone.MetalSynth().connect(crusher);
 	let x = document.getElementsByClassName("synthButton");
 	var i;
 	for (i = 0; i < x.length; i++) {
 	x[i].style.backgroundColor = "transparent";
-	}
-	document.querySelector('#Metal').style.backgroundColor = 'rgba(252, 186, 3, 0.75)';
-})
+	};
+	document.getElementById('Metal').style.backgroundColor = 'rgba(252, 186, 3, 0.75)';
+});
 
-document.querySelector('#FM').addEventListener('click', async () => {
+document.getElementById('FM').addEventListener('click', async () => {
 	synth = new window.Tone.FMSynth().connect(crusher);
 	let x = document.getElementsByClassName("synthButton");
 	var i;
 	for (i = 0; i < x.length; i++) {
 	x[i].style.backgroundColor = "transparent";
-	}
-	document.querySelector('#FM').style.backgroundColor = 'rgba(252, 186, 3, 0.75)';
-})
+	};
+	document.getElementById('FM').style.backgroundColor = 'rgba(252, 186, 3, 0.75)';
+});
 
 
 /*
@@ -64,37 +66,34 @@ Makes the keys clickable and plays the correct tone by catching the button ID
 song array for future playback
 */
 document.querySelectorAll('.natural').forEach(btn=>btn.addEventListener('click', async () => {
-	let note = "id"
+	let note = "id";
 	note = event.srcElement.id;
 	synth.triggerAttackRelease(note, "8n");
 	song.push(note);
 	localStorage.setItem("songStorage", JSON.stringify(song));
 	document.getElementById('synthScreen').innerHTML = song;
-}))
+}));
 
 document.querySelectorAll('.accidental').forEach(btn=>btn.addEventListener('click', async () => {
-	let note = "id"
+	let note = "id";
 	note = event.srcElement.id;
 	synth.triggerAttackRelease(note, "8n");
 	song.push(note);
 	localStorage.setItem("songStorage", JSON.stringify(song));
 	document.getElementById('synthScreen').innerHTML = song;
-}))
+}));
 
 // Plays back the recorded notes
 document.querySelector('#playBack').addEventListener('click', async () => {
 	for (var i = 0; i < song.length; i++) {
 			synth.triggerAttackRelease(song[i], "8n");
-	    await timer(500)
-	}
-})
+	    await timer(500);
+	};
+});
 
 // Clears the song from the screen
 document.querySelector('#clear').addEventListener('click', () => {
-	song = []
+	song = [];
 	localStorage.clear();
-	document.getElementById('synthScreen').innerHTML = song
-})
-
-let songStorage = document.getElementById('synthScreen').innerHTML
-songStorage = document.getElementById('synthScreen').innerHTML
+	document.getElementById('synthScreen').innerHTML = song;
+});
